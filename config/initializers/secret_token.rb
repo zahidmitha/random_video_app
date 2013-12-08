dbc03b09db70529bacdc5250c5eaae6ddf00623c
Zahid <kronosmes@gmail.com>
2013-12-08 01:37:30 +0000

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RandomVideo::Application.config.secret_token = 'f6d41424a074a237d192e3be6eecdbd09f53c48102066476503f456849d3bde9169153497b369359ecf7f83508a9af005b1e40fea3707204ab48c664cba1d9f9'
---
+++ keys.rb
@@ -0,0 +1,37 @@
+# Identity: 253214874441.apps.googleusercontent.com
+# Verified by: 253214874441.apps.googleusercontent.com
+# Expires: 05/05/23
+
+# Subject Name
+# CN (Common Name):	253214874441.apps.googleusercontent.com
+# Issuer Name
+# CN (Common Name):	253214874441.apps.googleusercontent.com
+# Issued Certificate
+# Version:	3
+# Serial Number:	7C 40 FE 81 7E 0E 79 84
+# Not Valid Before:	2013-05-07
+# Not Valid After:	2023-05-05
+# Certificate Fingerprints
+# SHA1:	DF E4 A5 96 88 27 91 82 44 E8 CD 44 56 40 E5 6A 93 73 4B 15
+# MD5:	57 F5 30 07 DD F9 B6 70 40 26 04 DD 55 78 06 C3
+# Public Key Info
+# Key Algorithm:	RSA
+# Key Parameters:	05 00
+# Key Size:	1024
+# Key SHA1 Fingerprint:	10 33 F7 44 E8 A2 4A 1D A2 00 44 A0 76 99 AE 35 F8 5D 5E AA
+# Public Key:	30 81 89 02 81 81 00 E8 9A F2 5E C5 D4 A1 E5 39 C8 AF 6B 4C 53 A2 23 F0 7C A5 FE 83 EE 29 AB F3 8F C8 4C 4E C9 6A F3 A3 79 B0 54 2F 07 ED 4F 00 55 5E E4 BD FD 08 06 C2 B2 7E 76 80 40 72 AF F6 5F AC 0A A2 FE 7E D8 71 5C 0A D5 C3 37 88 10 C1 7C E4 56 31 70 98 0F AE A7 8C 59 FE 32 6A 01 92 AB B6 57 EE 19 88 E9 E1 B5 2F 8E 30 B8 52 64 EB F4 7E EC 2A 31 68 23 6D 46 13 E3 27 6F 66 88 E2 C3 B7 1B 86 C7 4E C1 02 03 01 00 01
+# Basic Constraints
+# Certificate Authority:	No
+# Max Path Length:	Unlimited
+# Critical:	Yes
+# Key Usage
+# Usages:	Digital signature
+# Critical:	Yes
+# Extended Key Usage
+# Allowed Purposes:	Client Authentication
+# Critical:	Yes
+# Signature
+# Signature Algorithm:	SHA1 with RSA
+# Signature Parameters:	05 00
+# Signature:	4D D6 A9 F9 81 93 3D D2 3A 00 92 F3 18 DB DE 83 41 66 A5 E4 0E 23 3D 22 3B DD B4 69 83 D2 57 4B 5E B5 7E E3 53 3B 5F 36 85 79 8F E0 DD BB C2 1E D0 11 8D 57 CD 79 73 1B EB 9A EF 85 C4 EC 5E 5B FD 50 2C 36 2F 56 54 B1 0A 3F BB 81 D4 D3 AF B1 F4 25 F0 C3 5D C8 0F FD E3 03 A4 BA BA CB 57 CF 9D D4 63 B3 38 95 E7 6D 42 EE 56 EC E2 6A 77 76 F1 37 D6 96 74 85 5D 0F ED 8B D3 43 4A E5 97 F5
+
